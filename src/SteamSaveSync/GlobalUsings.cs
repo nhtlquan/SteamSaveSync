66daf073df80e753c5eaac7787a179237c78bf52
nhtlquan <nhtl1993@gmail.com>
2026-09-07 18:16:13 +0700

    Add HTTP global usings for LAN transfer

--- src/SteamSaveSync/GlobalUsings.cs
+++ src/SteamSaveSync/GlobalUsings.cs
@@ -4,3 +4,5 @@ global using System.Linq;
 global using System.Collections.Generic;
 global using System.Threading;
 global using System.Threading.Tasks;
+global using System.Net.Http;
+global using System.Net.Http.Json;
